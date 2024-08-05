86ed333359a015754928abde14b68c3e3a422400
joshysmart <sanijoshua10@gmail.com>
2024-08-05 22:05:51 +0100

    fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,10 @@
 export const sendEmail = (formData: FormData) => {
-  return fetch("https://formsubmit.co/0a575ddc45deae46842f14e8c019546c", {
+  return fetch("https://formsubmit.co/ajax/0a575ddc45deae46842f14e8c019546c", {
     method: "POST",
-    body: formData,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
   });
 };
