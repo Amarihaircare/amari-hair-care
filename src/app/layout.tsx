62389f8015bd5dbfc7ec68715474ec7ddba4a577
joshysmart <sanijoshua10@gmail.com>
2024-08-05 17:25:27 +0100

    remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${nunito.variable} ${montserrat.variable} flex flex-col items-center justify-center overflow-hidden`}
+        className={`${nunito.variable} ${montserrat.variable} flex flex-col items-center justify-center`}
       >
         <CartProvider>
           <Navbar />
