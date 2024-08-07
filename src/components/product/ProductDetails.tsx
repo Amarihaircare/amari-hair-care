6656da8c5ea24ddee24688be75dcef9e8071b627
joshysmart <sanijoshua10@gmail.com>
2024-08-07 10:26:59 +0100

    fix: build errors

--- src/components/product/ProductDetails.tsx
+++ src/components/product/ProductDetails.tsx
@@ -3,10 +3,10 @@
 import en from "@/language/en";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
-import Description from "./Description";
 import Reviews from "./Reviews";
 import AdditionalInformation from "./AdditionalInformation";
 import { TProduct } from "@/assets/data/products";
+import PurifiedHtml from "../shared/PurifiedHtml";
 
 interface ProductDetailsProps {
   description: string;
@@ -30,10 +30,10 @@ export default function ProductDetails({
   ];
 
   return (
-    <div className="bg-white border border-gray-200 shadow py-10 lg:py-20 px-4 flex flex-col items-center justify-center">
-      <div className="flex flex-col gap-8 lg:max-w-[70%] w-full">
-        <div className="lg:hidden flex flex-col gap-2 items-center">
-          <p className="font-semibold items-center text-center">
+    <div className="flex flex-col items-center justify-center border border-gray-200 bg-white px-4 py-10 shadow lg:py-20">
+      <div className="flex w-full flex-col gap-8 lg:max-w-[70%]">
+        <div className="flex flex-col items-center gap-2 lg:hidden">
+          <p className="items-center text-center font-semibold">
             {tabs[activeTab]}
           </p>
           <div className="flex items-center gap-2">
@@ -41,7 +41,7 @@ export default function ProductDetails({
               <button
                 key={index}
                 onClick={() => setActiveTab(index)}
-                className={cn("w-4 h-4 rounded-full bg-gray-200", {
+                className={cn("h-4 w-4 rounded-full bg-gray-200", {
                   "bg-primary": activeTab === index,
                 })}
               />
@@ -49,7 +49,7 @@ export default function ProductDetails({
           </div>
         </div>
 
-        <ul className="lg:grid grid-cols-3 hidden border-b border-gray-200 w-full">
+        <ul className="hidden w-full grid-cols-3 border-b border-gray-200 lg:grid">
           {tabs.map((tab, index) => (
             <li
               onClick={() => setActiveTab(index)}
@@ -59,7 +59,7 @@ export default function ProductDetails({
               aria-label="tab"
               key={index}
               className={cn(
-                "flex font-semibold items-center justify-center py-4 cursor-pointer text-center",
+                "flex cursor-pointer items-center justify-center py-4 text-center font-semibold",
                 {
                   "border-b-2 border-green-700 text-green-700":
                     activeTab === index,
@@ -70,7 +70,7 @@ export default function ProductDetails({
             </li>
           ))}
         </ul>
-        {activeTab === 0 && <Description description={description} />}
+        {activeTab === 0 && <PurifiedHtml html={description} />}
         {activeTab === 1 && <Reviews reviews={reviews} />}
         {activeTab === 2 && (
           <AdditionalInformation
--- src/components/shared/PurifiedHtml.tsx
+++ src/components/shared/PurifiedHtml.tsx
@@ -1,7 +1,19 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import DOMPurify from "dompurify";
 
 export default function PurifiedHtml({ html }: { html: string }) {
-  const sanitizedDescription = DOMPurify.sanitize(html);
-  return <div dangerouslySetInnerHTML={{ __html: sanitizedDescription }} />;
+  const [sanitizedHtml, setSanitizedHtml] = useState("");
+
+  useEffect(() => {
+    if (html.length === 0) return;
+
+    const domPurify = DOMPurify(window);
+    const cleanHtml = domPurify.sanitize(html);
+    setSanitizedHtml(cleanHtml);
+  }, [html]);
+
+  if (!sanitizedHtml) return <></>;
+
+  return <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }}></div>;
 }
